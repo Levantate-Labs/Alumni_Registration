2f6beabc619c3bfa194b1d49c0a5b33996f622f8
akhil-is-watching <praj2k2@protonmail.com>
2024-02-25 06:59:25 +0530

    Updated to cors again

--- main.go
+++ main.go
@@ -26,10 +26,9 @@ func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 	app.Use(cors.New(cors.Config{
-		AllowHeaders:     "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
-		AllowOrigins:     "*",
-		AllowCredentials: true,
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+		AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+		AllowOrigins: "*",
+		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
 	routes.InitRoutes(app)
 	config, err := config.LoadConfig()
